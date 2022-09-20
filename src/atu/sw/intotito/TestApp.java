9b3164c5f6052665d9bd863456aeec6f1faa8b30
intotito <intotito@gmail.com>
2022-09-20 10:35:23 +0100

    First Commit

---
+++ src/atu/sw/intotito/TestApp.java
@@ -0,0 +1,10 @@
+package atu.sw.intotito;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to OS Programming");
+	}
+
+}
